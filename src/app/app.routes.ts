7d14649ce18bcce1200467bfd77f43a5702caf50
ivangavlik <ivangavlik963@gmail.com>
2024-11-14 20:18:18 +0100

    routes index and tools basic component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { LayoutComponent } from './layout/layout.component';
+import { ToolsComponent } from './tools/tools.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {path: '', component: LandingComponent},
+  {path: 'tools', component: LayoutComponent,
+    children: [
+      { path: '', component: ToolsComponent },
+    ],
+  },
+  ];
