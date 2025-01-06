b1cc3ed3e711170ae1ea0d89fe98791119b74283
ivangavlik <ivangavlik963@gmail.com>
2025-01-06 17:32:34 +0100

    multistage api request

--- src/app/openai/openai.service.ts
+++ src/app/openai/openai.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import { OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import {Observable, switchMap} from 'rxjs';
 import { Lecture } from '../lecture-generator/lecture';
 
 @Injectable({
@@ -9,13 +9,65 @@ import { Lecture } from '../lecture-generator/lecture';
 })
 export class OpenaiService {
 
-   private apiUrl = 'https://thawing-cove-37000-15e18dd2b745.herokuapp.com';
-   private apiKey = ''; // Replace with your OpenAI API key
+   private baseUrl = 'https://teachers-center-be.onrender.com';
+//  private baseUrl = 'http://teachers-center-be.onrender.com/';
+
 
    constructor(private http: HttpClient) {}
 
    getChatCompletion(lecture: Lecture): Observable<any> {
-    return this.http.get(this.apiUrl + lecture.url(),  { responseType: 'text' });
+//    return this.http.get(this.apiUrl + lecture.url(),  { responseType: 'text' });
+
+     const queryParams = new HttpParams()
+       .set('language', lecture.language)
+       .set('language-level', lecture.languageLevel)
+       .set('topic', lecture.vocabularyTopic)
+       .set('topic-size', lecture.vocabularySize.toString())
+       .set('topic-questions', lecture.vocabularyQuestions.toString())
+       .set('grammar', lecture.grammarTopic)
+       .set('grammar-examples', lecture.grammarExamples.toString())
+       .set('grammar-exercises', lecture.grammarExercises.toString())
+       .set('homework', lecture.homework.toString())
+       .set('discussion', lecture.discussion.toString())
+       .set('dictionary', lecture.dictionary.toString())
+       .set('common-phrases', lecture.commonPhrases.toString());
+
+     return this.generateLectureText(queryParams).pipe(
+       switchMap((lectureTextResponse) =>
+         this.generateTextQuestions(lectureTextResponse, queryParams).pipe(
+           switchMap(() =>
+             this.generateGrammarExplanation(lectureTextResponse, queryParams)
+           )
+         )
+       )
+     );
+
+  }
+
+  // Method to call /generate-lecture/lecture-text
+  generateLectureText(queryParams: HttpParams): Observable<any> {
+    return this.http.post(`${this.baseUrl}/generate-lecture/lecture-text`, {}, {
+      params: queryParams,
+      responseType: 'text'
+    });
+  }
+
+  // Method to call /generate-lecture/text-questions
+  generateTextQuestions(lectureTextResponse: string, queryParams: HttpParams): Observable<any> {
+    return this.http.post(`${this.baseUrl}/generate-lecture/text-questions`,
+      { text: lectureTextResponse }, // Body includes the lectureTextResponse
+      { params: queryParams, responseType: 'text' }
+      );
+  }
+
+  // Method to call /generate-lecture/grammar-explanation
+  generateGrammarExplanation(
+    lectureTextResponse: string, queryParams: HttpParams
+  ): Observable<any> {
+    return this.http.post(`${this.baseUrl}/generate-lecture/grammar-explanation`,
+      { text: lectureTextResponse }, // Body includes the lectureTextResponse
+      { params: queryParams, responseType: 'text' }
+      );
   }
 
 }
